4dabc0b3acecdb3da186b2f083fadc93b6e086f1
Ed-s23 <laloangeles117@gmail.com>
2025-11-14 09:00:14 -0600

    sibicion correcciones

--- proyect/settings.py
+++ proyect/settings.py
@@ -28,11 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1:5173',
+    #TODo :añadir parte de render
 ]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
